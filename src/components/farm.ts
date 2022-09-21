7036f42bf6346b976006173d6f97ae0ffff44d87
Tim Leach <timleach635@gmail.com>
2022-09-21 17:07:57 +0100

    Move files up a level

--- src/components/farm.ts
+++ src/components/farm.ts
@@ -1,12 +1,12 @@
 import { defineComponent, ISchema, Types } from "bitecs";
 
-const FarmStates = {
-  0: "uncreated",
-  1: "plowed",
-  2: "growing",
-  3: "ready to harvest",
-  4: "harvested",
-};
+// const FarmStates = {
+//   0: "uncreated",
+//   1: "plowed",
+//   2: "growing",
+//   3: "ready to harvest",
+//   4: "harvested",
+// };
 
 const FarmSchema: ISchema = {
   state: Types.ui8,
--- src/engine.ts
+++ src/engine.ts
@@ -1,7 +1,7 @@
-import world, { World } from "./ecs/world";
-import { farmGrowthSystem } from "./ecs/systems/farm";
-import { autoHarvestSystem, generateAutoHarvestSystem } from "./ecs/systems/debug";
-import { timeSystem } from "./ecs/systems/time";
+import world, { World } from "./world";
+import { farmGrowthSystem } from "./systems/farm";
+import { generateAutoHarvestSystem } from "./systems/debug";
+import { timeSystem } from "./systems/time";
 
 export function initialise(
   harvestCallback: (farmId: number, cropId: number) => void
